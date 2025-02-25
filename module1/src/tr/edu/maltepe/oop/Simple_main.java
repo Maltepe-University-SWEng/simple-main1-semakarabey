02e6d3cad06cd4ef10c7ed7b7a384277208244fb
sema karabey <karabeysema22@gmail.com>
2025-02-25 14:35:09 +0300

    220704023

--- module1/src/tr/edu/maltepe/oop/Simple_main.java
+++ module1/src/tr/edu/maltepe/oop/Simple_main.java
@@ -3,7 +3,7 @@ public  class Simple_main {
 
  public static void main(String []args){
         //
-       System.out.println("simple main.....");
+       System.out.println("Sema Karabey");
         }
 }
 
